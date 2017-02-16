d1b01c3cab582690a71ccf65dc6a5b7508ccbe86
mercier1 <mercier1@mail.ru>
2017-02-16 08:50:42 +0400

    nnn

--- src/Module03/Home3_1/Main.java
+++ src/Module03/Home3_1/Main.java
@@ -2,10 +2,29 @@ package Module03.Home3_1;
 
 public class Main {
     public static void main(String... args) {
-        Bird birdVoody = new Bird("voody");
-        birdVoody.sing("voody");
+        Bird bird1 = new Bird("voody");
+        bird1.sing();
         System.out.println();
         Bird bird = new Bird();
-        bird.sing("bird");
+        bird.sing();
+    }
+}
+
+class Bird {
+    private String name;
+
+    public Bird() {
+    }
+
+    public void sing() {
+        System.out.println("I am walking");
+        System.out.println("I am flying");
+        System.out.println("I am singing");
+        System.out.println("I am Bird");
+    }
+
+    public Bird(String name) {
+        if (name == "voody")
+            System.out.println("I am walking " + name);
     }
 }
